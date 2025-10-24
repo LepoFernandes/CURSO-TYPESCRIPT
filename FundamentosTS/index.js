03601c79680bfab0e329c4f132be73804a9739ac
Lucas Fernandes <lcsfernandesg@gmail.com>
2025-10-23 21:00:28 -0300

    update

---
+++ FundamentosTS/index.js
@@ -0,0 +1,29 @@
+//AULA - 1 = NUMBERS
+console.log("AULA 1");
+console.log(" ");
+var x1 = 10;
+console.log(x1);
+console.log(typeof x1);
+var y = 15.59826;
+console.log(y);
+console.log(typeof y);
+//AULA - 2 = STRING
+console.log(" ");
+console.log("AULA 2");
+console.log(" ");
+var firstName1 = "Lucas";
+console.log(firstName1.toUpperCase());
+var fullName;
+var lastName1 = "Fernandes";
+fullName = firstName1 + " " + lastName1;
+console.log(fullName);
+console.log(typeof fullName);
+//AULA - 3 = BOOLEAN
+console.log(" ");
+console.log("AULA 3");
+console.log(" ");
+var a = false;
+console.log(a);
+console.log(typeof a);
+a = true;
+console.log(a);
---
+++ FundamentosTS/index.ts
@@ -0,0 +1,47 @@
+//AULA - 1 = NUMBERS
+console.log("AULA 1")
+console.log(" ")
+
+let x1: number = 10
+console.log(x1)
+console.log(typeof x1)
+
+
+const y:number = 15.59826
+console.log(y)
+console.log(typeof y)
+
+//AULA - 2 = STRING
+console.log(" ")
+console.log("AULA 2")
+console.log(" ")
+
+const firstName1 : string = "Lucas"
+console.log(firstName1.toUpperCase())
+
+let fullName: string 
+const lastName1 : string = "Fernandes"
+
+fullName = firstName1 + " " + lastName1
+
+console.log(fullName)
+console.log(typeof fullName)
+
+//AULA - 3 = BOOLEAN
+console.log(" ")
+console.log("AULA 3")
+console.log(" ")
+
+let a: boolean = false
+
+console.log(a)
+console.log(typeof a)
+
+a = true 
+
+console.log(a)
+
+//AULA - 4 = BOOLEAN
+console.log(" ")
+console.log("AULA 4")
+console.log(" ")
