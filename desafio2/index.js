faebecfc507240e68c8710d390bbad9c8c7e079c
Lucas Fernandes <lcsfernandesg@gmail.com>
2025-10-24 13:57:39 -0300

    new challenge

---
+++ desafio2/index.js
@@ -0,0 +1,3 @@
+var age = 25;
+var ageConvert = age.toString();
+console.log("A idade sera: " + ageConvert + " anos.");
---
+++ desafio2/index.ts
@@ -0,0 +1,5 @@
+let age: number = 25
+
+let ageConvert = age.toString()
+
+console.log("A idade sera: " + ageConvert + " anos.")
