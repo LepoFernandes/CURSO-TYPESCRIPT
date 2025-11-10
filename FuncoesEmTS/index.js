c039bbbe9ca2b7243dbd2e3c041a77a671de4343
Lucas Fernandes <lcsfernandesg@gmail.com>
2025-11-10 11:31:23 -0300

    update

--- FuncoesEmTS/index.js
+++ FuncoesEmTS/index.js
@@ -1,4 +1,21 @@
-//AULA 1 - 
+//AULA 1 - VOID
 console.log("");
 console.log("AULA 1");
 console.log("");
+function withoutReturn() {
+    console.log("Funcao sem retorno");
+}
+withoutReturn(); //MUITO UTILIZADA EM FUNCOES QUE MEXEM COM O DOM
+//AULA 2 - CALLBACK COMO ARGUMENTO
+console.log("");
+console.log("AULA 2");
+console.log("");
+function greeting(name) {
+    return "Hello ".concat(name);
+}
+function preGreeting(f, username) {
+    console.log("Preparando a funcao");
+    var greet = f(username);
+    console.log(greet);
+}
+preGreeting(greeting, "Matheus");
--- FuncoesEmTS/index.ts
+++ FuncoesEmTS/index.ts
@@ -1,4 +1,30 @@
-//AULA 1 - 
+//AULA 1 - VOID
 console.log("")
 console.log("AULA 1")
 console.log("")
+
+function withoutReturn(): void{
+    console.log("Funcao sem retorno")
+}
+
+withoutReturn() //MUITO UTILIZADA EM FUNCOES QUE MEXEM COM O DOM
+
+
+//AULA 2 - CALLBACK COMO ARGUMENTO
+console.log("")
+console.log("AULA 2")
+console.log("")
+
+function greeting(name: string){
+    return `Hello ${name}`
+}
+
+function preGreeting(f: (name: string) => string, username: string){
+    console.log("Preparando a funcao")
+
+    const greet = f(username)
+
+    console.log(greet)
+}
+
+preGreeting(greeting, "Matheus")
