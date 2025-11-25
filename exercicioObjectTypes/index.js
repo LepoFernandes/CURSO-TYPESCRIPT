2fc82200e357b4682e3b822e3411f40acc56b6c9
Lucas Fernandes <lcsfernandesg@gmail.com>
2025-11-25 13:40:28 -0300

    update

--- exercicioObjectTypes/index.js
+++ exercicioObjectTypes/index.js
@@ -38,3 +38,80 @@ printUser(userA);
 printUser(userB);
 printUser(userC);
 printUser(userD);
+console.log("NOVO EXERCICIO");
+console.log("NOVO EXERCICIO");
+console.log("NOVO EXERCICIO");
+function showUserConfig(user) {
+    console.log("");
+    console.log("User config: ");
+    for (var index in user.config) {
+        console.log("".concat(index, ": ").concat(user.config[index]));
+    }
+}
+var user1 = {
+    config: {
+        id: 10,
+        username: "Lucas",
+        darkMode: true,
+        theme: "blue"
+    }
+};
+var user2 = {
+    config: {
+        id: 17,
+        username: "Vitor",
+        notifications: false,
+        volume: 15
+    }
+};
+var user3 = {
+    config: {
+        id: 529,
+        username: "Pedro",
+        language: "Portuguese",
+        theme: "pink"
+    }
+};
+var user4 = {
+    config: {
+        id: 485,
+        username: "Murilo"
+    }
+};
+showUserConfig(user1);
+showUserConfig(user2);
+showUserConfig(user3);
+showUserConfig(user4);
+console.log("NOVO EXERCICIO");
+console.log("NOVO EXERCICIO");
+console.log("NOVO EXERCICIO");
+function printUserInfo(user) {
+    console.log("");
+    console.log("Users info:");
+    for (var item in user) {
+        console.log("".concat(item, ": ").concat(user[item]));
+    }
+}
+var admin = {
+    id: 1,
+    name: "Lucas",
+    permissions: true,
+    deleteUsers: true
+};
+var regular = {
+    id: 2,
+    name: "Raquel",
+    subscriptionType: "premium",
+    postsCreated: 42,
+    profileComplete: true
+};
+var guest = {
+    id: 3,
+    name: "Murilo",
+    temporary: true,
+    expiresAt: "2025-12-01",
+    acessLimited: true
+};
+printUserInfo(admin);
+printUserInfo(regular);
+printUserInfo(guest);
--- exercicioObjectTypes/index.ts
+++ exercicioObjectTypes/index.ts
@@ -55,3 +55,130 @@ printUser(userA)
 printUser(userB)
 printUser(userC)
 printUser(userD)
+
+
+console.log("NOVO EXERCICIO")
+console.log("NOVO EXERCICIO")
+console.log("NOVO EXERCICIO")
+
+
+interface baseConfig{
+    [index: string] : number | string | boolean
+    id : number
+    username: string
+}
+
+
+interface userConfig <T extends baseConfig>{
+    config: T
+}
+
+function showUserConfig<T extends baseConfig>(user: userConfig<T>){
+    console.log("")
+    console.log("User config: ")
+
+   for(const index in user.config){
+    console.log(`${index}: ${user.config[index]}`)
+   }
+}
+
+const user1: userConfig<baseConfig> = {
+    config: {
+    id: 10,
+    username: "Lucas",
+    darkMode: true,
+    theme: "blue"
+}};
+
+const user2: userConfig<baseConfig> = {
+    config:{
+    id: 17,
+    username: "Vitor",
+    notifications: false,
+    volume: 15
+}};
+
+const user3: userConfig<baseConfig> = {
+    config:{
+    id: 529,
+    username: "Pedro",
+    language: "Portuguese",
+    theme: "pink"
+}};
+
+const user4: userConfig<baseConfig> = {
+    config:{
+    id: 485,
+    username: "Murilo"
+}};
+
+showUserConfig(user1)
+showUserConfig(user2)
+showUserConfig(user3)
+showUserConfig(user4)
+
+
+console.log("NOVO EXERCICIO")
+console.log("NOVO EXERCICIO")
+console.log("NOVO EXERCICIO")
+
+
+interface basicUser {
+    [index: string] : boolean | string | number
+    id: number
+    name: string
+}
+
+interface adminUser extends basicUser{
+    permissions: boolean
+    deleteUsers: boolean
+}
+
+interface regularUser extends basicUser{
+    subscriptionType: "free" | "premium"
+    postsCreated: number
+    profileComplete: boolean
+}
+
+interface guestUser extends basicUser{
+    temporary: boolean
+    expiresAt: string
+    acessLimited: boolean
+}
+
+function printUserInfo(user: basicUser){
+    console.log("")
+    console.log("Users info:")
+
+    for(const item in user){
+        console.log(`${item}: ${user[item]}`)
+    }
+}
+
+
+const admin: adminUser = {
+    id: 1,
+    name: "Lucas",
+    permissions: true,
+    deleteUsers: true
+};
+
+const regular: regularUser = {
+    id: 2,
+    name: "Raquel",
+    subscriptionType: "premium",
+    postsCreated: 42,
+    profileComplete: true
+};
+
+const guest: guestUser = {
+    id: 3,
+    name: "Murilo",
+    temporary: true,
+    expiresAt: "2025-12-01",
+    acessLimited: true
+};
+
+printUserInfo(admin)
+printUserInfo(regular)
+printUserInfo(guest)
