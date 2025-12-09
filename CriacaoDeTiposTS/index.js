42c7d74a3419c537c7f1d03f6b90013d3ef2a4a5
Lucas Fernandes <lcsfernandesg@gmail.com>
2025-12-09 13:47:18 -0300

    update

--- CriacaoDeTiposTS/index.js
+++ CriacaoDeTiposTS/index.js
@@ -29,3 +29,58 @@ var myCar = { name: "Fusca", wheels: 4, engine: 2.0, color: "Branco" };
 var myPen = { name: "Caneta BIC", wheels: false, engine: false, color: "Azul" };
 console.log(myCar);
 console.log(myPen);
+//AULA 4 - revisao de type parameters
+console.log("");
+console.log("AULA 4 - TYPE PARAMETERS");
+console.log("");
+function getSomeKey(obj, key) {
+    return "A chave ".concat(String(key), " esta presente no objeto e tem valor de ").concat(String(obj[key]));
+}
+var server = {
+    HD: '2TB',
+    RAM: '32GB'
+};
+console.log(getSomeKey(server, 'RAM'));
+//AULA 5 - keyof Type Operator
+console.log("");
+console.log("AULA 5 - keyof TYPE OPERATOR");
+console.log("");
+function showCharName(obj, key) {
+    return "".concat(obj[key]);
+}
+var myChar = {
+    name: "Lucas",
+    age: 26,
+    driveLicense: true
+};
+console.log(showCharName(myChar, 'name'));
+//AULA 6 - typeof Type Operator
+console.log("");
+console.log("AULA 6 - typeof TYPE OPERATOR");
+console.log("");
+var userName = "Lucas";
+var userName2 = "Lepo";
+var userName3 = "Fernandes";
+//AULA 7 - Indexed acess types
+console.log("");
+console.log("AULA 7 - INDEXED ACCESS TYPES");
+console.log("");
+var newTruck = {
+    km: 10000,
+    kg: 5000,
+    description: "Caminhao pequeno porte"
+};
+function showKM(km) {
+    console.log("O veiculo tem a km de : ".concat(km));
+}
+showKM(newTruck.km);
+//AULA 8 - Conditional types
+console.log("");
+console.log("AULA 8 - CONDITIONAL TYPES");
+console.log("");
+var someVar = 5; //funciona ja que o B realmente tem extensao de A 
+//AULA 9 - Template Literals
+console.log("");
+console.log("AULA 9 - TEMPLATE LITERALS TYPE");
+console.log("");
+var testing = "some text";
--- CriacaoDeTiposTS/index.ts
+++ CriacaoDeTiposTS/index.ts
@@ -52,3 +52,114 @@ const myPen:Pen = {name: "Caneta BIC", wheels: false, engine: false, color: "Azu
 
 console.log(myCar)
 console.log(myPen)
+
+//AULA 4 - revisao de type parameters
+console.log("")
+console.log("AULA 4 - TYPE PARAMETERS")
+console.log("")
+
+function getSomeKey<T, K extends keyof T>(obj: T, key: K) {
+    return `A chave ${String(key)} esta presente no objeto e tem valor de ${String(obj[key])}`
+}
+
+const server = {
+    HD : '2TB',
+    RAM: '32GB'
+}
+
+console.log(getSomeKey(server, 'RAM'))
+
+
+//AULA 5 - keyof Type Operator
+console.log("")
+console.log("AULA 5 - keyof TYPE OPERATOR")
+console.log("")
+
+
+type Character = {name: string, age: number, driveLicense: boolean}
+
+type C = keyof Character
+
+function showCharName(obj: Character, key: C): string{
+    return `${obj[key]}`
+}
+
+const myChar: Character = {
+    name: "Lucas",
+    age: 26,
+    driveLicense: true
+}
+
+console.log(showCharName(myChar, 'name'))
+
+
+//AULA 6 - typeof Type Operator
+console.log("")
+console.log("AULA 6 - typeof TYPE OPERATOR")
+console.log("")
+
+const userName : string = "Lucas"
+const userName2 : typeof userName = "Lepo"
+
+type x = typeof userName
+const userName3 : x = "Fernandes"
+
+
+//AULA 7 - Indexed acess types
+console.log("")
+console.log("AULA 7 - INDEXED ACCESS TYPES")
+console.log("")
+
+type Truck = {
+    km: number
+    kg: number
+    description: string
+}
+
+type Km = Truck['km']
+
+const newTruck: Truck ={
+    km: 10000,
+    kg: 5000,
+    description: "Caminhao pequeno porte"
+}
+
+function showKM(km: Km){
+    console.log(`O veiculo tem a km de : ${km}`)
+}
+
+
+showKM(newTruck.km)
+
+
+//AULA 8 - Conditional types
+console.log("")
+console.log("AULA 8 - CONDITIONAL TYPES")
+console.log("")
+
+interface A {}
+interface B extends A {}
+
+type myType = B extends A ? number : string
+
+
+const someVar: myType = 5 //funciona ja que o B realmente tem extensao de A 
+
+
+//AULA 9 - Template Literals
+console.log("")
+console.log("AULA 9 - TEMPLATE LITERALS TYPE")
+console.log("")
+
+
+type testA = "text"
+
+type customType = `some ${testA}`
+
+const testing: customType = "some text"
+
+
+type a1 = "Testando"
+type a2 = "Union"
+
+type a3 = `${a1}` | `${a2}`
