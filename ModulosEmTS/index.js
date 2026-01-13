ebf56bf38272606fe70b37d149e6580a527365cf
Lucas Fernandes <lcsfernandesg@gmail.com>
2026-01-13 19:45:18 -0300

    update

--- ModulosEmTS/index.js
+++ ModulosEmTS/index.js
@@ -11,3 +11,18 @@ var multiple_1 = require("./multiple");
 console.log(multiple_1.a);
 console.log(multiple_1.b);
 (0, multiple_1.myFunction)();
+// 4 - ALIAS
+var changename_1 = require("./changename");
+console.log(changename_1.someName);
+// 5 - IMPORTANDO DIVERSOS ITENS
+var myNumbers = require("./numbers"); //PRECISA DE UM ALIAS "AS" PRA FUNCIONAR
+console.log(myNumbers);
+var User = /** @class */ (function () {
+    function User(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return User;
+}());
+var joao = new User("joao", 25);
+console.log(joao);
--- ModulosEmTS/index.ts
+++ ModulosEmTS/index.ts
@@ -17,3 +17,32 @@ import {a,b,myFunction} from './multiple'
 console.log(a)
 console.log(b)
 myFunction();
+
+// 4 - ALIAS
+
+import {someName as name} from "./changename"
+
+console.log(name)
+
+// 5 - IMPORTANDO DIVERSOS ITENS
+
+import * as myNumbers from "./numbers"  //PRECISA DE UM ALIAS "AS" PRA FUNCIONAR
+
+console.log(myNumbers)
+
+// 6 - IMPORTANDO TIPOS 
+
+import {Human} from "./mytype"
+
+class User implements Human {
+    name 
+    age
+
+    constructor (name: string, age: number){
+        this.name = name
+        this.age = age
+    }
+}
+
+const joao = new User("joao", 25)
+console.log(joao)
