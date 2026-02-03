2f2cf08290310b0a819d78461dec73b9cb921678
Lucas Fernandes <lcsfernandesg@gmail.com>
2026-02-03 14:00:31 -0300

    update

--- react_com_ts/src/App.tsx
+++ react_com_ts/src/App.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 
+
+//4 - IMPORTACAO DE COMPONENTES
+
+import FirstComponent from './components/FIrstComponent';
+
 function App() {
+
+  //1 - VARIAVEIS
+  const name: string = "Lucas"
+  const age: number = 26
+  const isWorking: boolean = true
+
+
+  //2 - FUNCOES
+
+  const userGreeting = (name:string) : string => {
+    return `Ola, ${name}`
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>
           TypeScript Com React
         </h1>
+        <h2>Nome: {name}</h2>
+        <p>Idade: {age}</p>
+        {isWorking && (
+          <p>Esta trabalhando!</p>
+        ) }
+        <h3>{userGreeting(name)}</h3>
+        <FirstComponent />
       </header>
     </div>
   );
---
+++ react_com_ts/src/components/FIrstComponent.tsx
@@ -0,0 +1,14 @@
+//3 - COMPONENTE
+import React from 'react'
+
+type Props = {}
+
+function FIrstComponent() {
+  return (
+    <div>
+        <h1>Meu primeiro componente</h1>
+    </div>
+  )
+}
+
+export default FIrstComponent
