73a728b0e5eca7d35c71afaecb0eae8a40e00d0d
Lucas Fernandes <lcsfernandesg@gmail.com>
2026-02-10 11:02:43 -0300

    inicio_projeto_curso_to-do

---
+++ todo_react/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+// components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import TaskForm from './components/TaskForm';
+
+//CSS
+import styles from './App.module.css'
+import TaskList from './components/TaskList';
+
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className={styles.main}>
+        <div>
+          <h2>O que voce vai fazer?</h2>
+          <TaskForm btnText="Criar Tarefa" />
+        </div>
+        <div>
+          <h2>Suas Tarefas</h2>
+          <TaskList />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo_react/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import style from './Footer.module.css'
+
+
+const Footer = () => {
+  return (
+    <footer className={style.footer}>
+        <p>
+          <span>React + TS ToDo</span> @ 2026
+        </p>
+      </footer>
+  )
+}
+
+export default Footer
