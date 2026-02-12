f95244b1f2cc2205986167e90819f80392f7dbc4
Lucas Fernandes <lcsfernandesg@gmail.com>
2026-02-12 11:50:39 -0300

    update

--- todo_react/src/components/TaskList.tsx
+++ todo_react/src/components/TaskList.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
 
-type Props = {}
+//INTERFACE
+import { ITask } from '../interface/Task'
 
-const TaskList = (props: Props) => {
-  return (
-    <div>Lista de Tarefas</div>
-  )
+//CSS
+import styles from './TaskList.module.css'
+
+
+type Props = 
+{
+  taskList: ITask[]
 }
+
+const TaskList = ({taskList}: Props) => {
+  return (
+    <>
+      {taskList.length > 0 ?(
+        taskList.map((task) => (
+          <div key={task.id}>
+            <div>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div>
+              <i className='bi bi-pencil'></i>
+              <i className='bi bi-trash'></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas!</p>
+      )}
+    </>
+  );
+};
 
 export default TaskList
